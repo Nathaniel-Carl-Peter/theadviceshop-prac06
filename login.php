9a255771d143401e79bf8b0f50d9b89682b3f14e
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2025-07-20 19:14:13 +1000

    Created if-else in login

--- login.php
+++ login.php
@@ -3,5 +3,10 @@ error_reporting(E_ALL);
 session_start();
 $_SESSION['username'] = $_REQUEST['username'];
 //echo $_GET['page'];
+if ($_REQUEST['username'] == '') {
+  echo'Username must be filled';
+  exit();
+}
+
 header("Location: ".$_GET['page'].".php");
 exit();
